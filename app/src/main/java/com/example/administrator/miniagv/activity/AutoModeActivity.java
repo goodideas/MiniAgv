9377a45d81601e1613cf5ef6c48205f633d857c4
kevin <441325961@qq.com>
2016-06-27 15:47:43 +0800

    建立包，分类。

---
+++ app/src/main/java/com/example/administrator/miniagv/activity/AutoModeActivity.java
@@ -0,0 +1,15 @@
+package com.example.administrator.miniagv.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.administrator.miniagv.R;
+
+public class AutoModeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auto_mode);
+    }
+}
---
+++ app/src/main/java/com/example/administrator/miniagv/activity/ProgrammedModeActivity.java
@@ -0,0 +1,15 @@
+package com.example.administrator.miniagv.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.administrator.miniagv.R;
+
+public class ProgrammedModeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_programmed_mode);
+    }
+}
